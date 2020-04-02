24ab8c770f9e4a79027dd8c9a0a2fd3f38c2281b
jsneider05 <joan_sneider@hotmail.com>
2020-04-02 18:11:02 -0500

    Manejo de strings

---
+++ clase2.js
@@ -0,0 +1,19 @@
+var nombre = 'Joan', apellido = 'Ardila'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = nombre + '  ' + apellido
+
+var nombreCompletoInterpolacionDeTexto = `${nombre} ${apellido.toUpperCase()}`
+
+var str = nombre.charAt(1) + nombre.charAt(2)
+
+var subStr = nombre.substr(1, 2)
+
+var ultimaLetraDelNombre = nombre.substr(-1, 1)
